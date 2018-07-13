9f395e0c6dba3fa7844ed6ceb66ae18bf32f98eb
Deepali2 <hdgarg@gmail.com>
2018-07-13 15:13:27 -0700

    creates a class called Repo and a function to save a repo instance of the class

--- database/index.js
+++ database/index.js
@@ -1,16 +1,31 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected!");
+});
+
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  // TODO: your schema here!  
+  repoName: String,
+  ownerLogin: String,
+  htmlUrl: String,
+  Description: String,
+  stargazers_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+
+
+let save = (err, repo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  if (err) return console.error(err);
+  console.log(repo);
 }
 
 module.exports.save = save;
