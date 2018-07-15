eaa0135dd822f355bc1960ed9e9031a82f963a73
Deepali2 <hdgarg@gmail.com>
2018-07-14 18:04:02 -0700

    renders the dummy data beautifully onto the DOM with no errors

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,22 +4,32 @@ const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     <p>There are {props.repos.length} repos.</p>
-    <div>      
-      {props.repos.map(repo => 
-         <TableRow row={repo}/>
+    <table> 
+      <tr>
+        <th>Repository Name</th> 
+        <th>Owner's Github Handle </th> 
+        <th>Owner's HTML URL</th> 
+        <th>Description</th> 
+        <th>Stargazers_count</th> 
+      </tr> 
+         
+      {props.repos.map((repo, index) => 
+         <TableRow row={repo} key={index}/>
       )}
-    </div>
+    </table>
   </div>
 )
 
 const TableRow = (props) => (
-  <div>
-    <td key={props.row.name}>{props.row.name}</td>
-    <td key={props.row.owner.login}>{props.row.owner.login}</td>
-    <td key={props.row.owner.html_url}>{props.row.owner.html_url}</td>
-    <td key={props.row.Description}>{props.row.Description}</td>
-    <td key={props.row.stargazers_count}>{props.row.stargazers_count}</td>    
-  </div>
+  <tbody>
+    <tr>
+      <td key={props.row.name}>{props.row.name}</td>
+      <td key={props.row.owner.login}>{props.row.owner.login}</td>
+      <td key={props.row.owner.html_url}>{props.row.owner.html_url}</td>
+      <td key={props.row.Description}>{props.row.Description}</td>
+      <td key={props.row.stargazers_count}>{props.row.stargazers_count}</td>    
+    </tr>
+  </tbody>
 )
 
 export default RepoList;
